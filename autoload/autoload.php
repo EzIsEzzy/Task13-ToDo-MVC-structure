56e7f9f7b05cc6245ee4472be4d77ccd398f664c
EzIsEzzy <ez.is.ezzy@gmail.com>
2024-09-26 15:52:29 +0300

    Modified the autooad file

--- autoload/autoload.php
+++ autoload/autoload.php
@@ -5,4 +5,5 @@ require_once './Controllers/RouteController.php';
 require_once './Controllers/NotesController.php';
 
 $PATH = $_SERVER['REQUEST_URI'];
-$root = '/SummerTraining/Task12-TODO_Website-main/';
+$root = '/SummerTraining/Task13_MVC/';
+//In case it runs on a server from the root directly, such as localhost:port/root, no need for the $root variable, otherwise, modify it to the path that it shows, here it is localhost:port/SummerTraining/Task13_MVC
